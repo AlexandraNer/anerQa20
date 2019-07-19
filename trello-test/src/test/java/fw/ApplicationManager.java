45deeea3acdabb8b7044f2fabaf8cfb397f4f713
AlexandraNer <51996302+AlexandraNer@users.noreply.github.com>
2019-07-19 11:28:30 +0300

    save

--- trello-test/src/test/java/fw/ApplicationManager.java
+++ trello-test/src/test/java/fw/ApplicationManager.java
@@ -1,12 +1,17 @@
 package fw;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    String browser;
     Board board ;
     SessionHelper session;
     Teams teams;
@@ -14,7 +19,23 @@ public class ApplicationManager {
     HelperLogOut logOut;
     WebDriver driver;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public void init() throws InterruptedException {
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            driver = new FirefoxDriver();
+        }else  if(browser.equals(BrowserType.CHROME)){
+            driver = new ChromeDriver();
+        }else  if(browser.equals(BrowserType.EDGE)){
+            driver = new EdgeDriver();
+        }else  if(browser.equals(BrowserType.IEXPLORE)){
+            driver = new InternetExplorerDriver();
+        }else {
+            System.out.println("Unknown browser format");
+        }
         driver = new FirefoxDriver();
         driver.get("https://trello.com/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
--- trello-test/src/test/java/tests/TestBase.java
+++ trello-test/src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import fw.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -8,7 +9,7 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-   static  ApplicationManager app = new ApplicationManager();
+    static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeSuite
     public void setUp() throws InterruptedException {
