68781f072ca51b8c480753fbc5e6d60dba2827f8
AlexandraNer <51996302+AlexandraNer@users.noreply.github.com>
2019-07-31 21:22:35 +0300

    create LoggerClass

--- trello-test/src/test/java/com/telRan/model/fw/ApplicationManager.java
+++ trello-test/src/test/java/com/telRan/model/fw/ApplicationManager.java
@@ -1,12 +1,16 @@
 package com.telRan.model.fw;
 
 import com.telRan.model.model.User;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,26 +21,40 @@ public class ApplicationManager {
     SessionHelper session;
     UserHelper user;
     Teams teams;
-    WebDriver driver;
+    EventFiringWebDriver driver;
     String browser;
+    public static  class MyListener extends AbstractWebDriverEventListener{
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by + "found");
+        }
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            System.out.println(throwable);
+        }
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println("Start search" + by);
+        }
+    }
 
     public ApplicationManager(String browser) {
         this.browser = browser;
     }
-
     public void init() throws InterruptedException {
 
         if (browser.equals(BrowserType.FIREFOX)) {
-            driver = new FirefoxDriver();
+            driver = new EventFiringWebDriver(new FirefoxDriver());
         }else  if(browser.equals(BrowserType.CHROME)){
-            driver = new ChromeDriver();
+            driver = new EventFiringWebDriver(new ChromeDriver());
         }else  if(browser.equals(BrowserType.EDGE)){
-            driver = new EdgeDriver();
+            driver = new EventFiringWebDriver(new EdgeDriver());
         }else  if(browser.equals(BrowserType.IEXPLORE)){
-            driver = new InternetExplorerDriver();
+            driver = new EventFiringWebDriver(new InternetExplorerDriver());
         }else {
             System.out.println("Unknown browser format");
         }
+        driver.register(new MyListener());
         openSite("https://trello.com/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         session=new SessionHelper(driver);
@@ -44,9 +62,7 @@ public class ApplicationManager {
         session.login(new User().withEmail("narishka@walla.com").withPassword("202010Zxc"));
         board = new Board(driver);
         teams = new Teams(driver);
-
     }
-
     public void openSite(String url) {
         driver.get(url);
     }
@@ -54,10 +70,7 @@ public class ApplicationManager {
     public void stop() {
         driver.quit();
     }
-
     //-------------------------------------------------------------------------------------------------------------
-
-
     public Board getBoard() {
         return board;
     }
--- trello-test/src/test/java/com/telRan/model/tests/EditUserDataTests.java
+++ trello-test/src/test/java/com/telRan/model/tests/EditUserDataTests.java
@@ -1,14 +1,16 @@
 package com.telRan.model.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 public class EditUserDataTests extends TestBase{
+
 
     @Test
     public void changeAvatarTest() throws InterruptedException {
         app.getUser().clickOnAvatar();
         app.getUser().enterToProfile();
         app.getUser().changeAvatar();
-        app.getUser().saveChanger();
     }
 }
